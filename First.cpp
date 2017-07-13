057bd1c6d8080107b064ee86086b98228b6fbe46
willhua <znjxboy@163.com>
2017-07-13 21:51:17 +0800

    使用Uniform实现颜色渐变的三角形

--- First.cpp
+++ First.cpp
@@ -3,12 +3,36 @@
 
 #include <iostream>
 
+using namespace std;
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+
+char *vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;\n"
+"void main(){\n"
+"gl_Position = vec4(pos, 1);\n"
+"}\n\0";
+
+
+char *fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 fragmentcolor;\n"
+"uniform vec4 green;\n"
+"void main(){\n"
+"fragmentcolor = green;\n"
+"}\n\0";
+
+
+
+
 
 int main()
 {
@@ -18,7 +42,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    
     // glfw window creation
     // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
@@ -39,6 +63,67 @@ int main()
         return -1;
     }
 
+
+
+    //创建顶点数据
+    float vertex[] = {
+        0,  0.5,    0,
+        -0.5, -0.5, 0,
+        0.5,  -0.5, 0
+    };
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  //要记得ENABLE
+    glBindVertexArray(VAO);
+
+
+
+    int success;
+    char loginfo[512];
+
+    //创建顶点着色器
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, loginfo);
+        cout << "vertexshader compile fail:" << loginfo << endl;
+    }
+
+    //创建片段着色器
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, 0, loginfo);
+        cout << "fragment compile fail:" << loginfo << endl;
+    }
+
+    //创建program
+    int program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, loginfo);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << loginfo << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -46,6 +131,17 @@ int main()
         // input
         // -----
         processInput(window);
+
+        glClearColor(0.5, 0, 0.5, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+        float time = glfwGetTime();
+        float green = sin(time) / 2.0 + 0.5;
+        int uniformPos = glGetUniformLocation(program, "green");
+        glUniform4f(uniformPos, 0, green, 0, 1);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
